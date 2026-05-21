1be04eda412903b4c24e3cbf4ac6d474f6746318
Akash R <akashr.devx@gmail.com>
2026-05-21 12:39:18 +0530

    feat: setup react with tailwind and initial components

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="flex h-screen bg-background">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/VaultHealth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VaultHealth = () => {
+  return (
+    <div>VaultHealth</div>
+  )
+}
+
+export default VaultHealth
